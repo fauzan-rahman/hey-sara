adcc5d4a4e2f5e470ff78c43f845fada80f8f58f
Fauzan Rahman <rahmanfauzanaulia@gmail.com>
2024-01-27 18:47:28 -0800

    make the script.js has 200 quotes

--- script.js
+++ script.js
@@ -203,6 +203,210 @@ const quotes = [
 
   "It's okay to not be okay",
 
+  //this is the 100 mark
+
+  "I meant what I said and I said what I meant. An elephant's faithful one-hundred percent. You'll be okay!",
+
+  "You know, the brick walls are there for a reason. The brick walls are not there to keep us out. The brick walls are there to give us a chance to show how badly we want something. Because the brick walls are there to stop the people who don't want it badly enough. They're there to stop the other people.",
+  
+  "Do not let the memories of your past limit the potential of your future. There are no limits to what you can achieve on your journey through life, except in your mind.",
+  
+  "please believe that things are good with me, and even when they're not, they will be soon enough. And i will always believe the same about you",
+  
+  "Know that life is never about looking over someone's shoulders",
+  
+  "Its okay if we don't want to be a genius, we have enough problems just trying to be a living person",
+
+  "Life is too short to waste any amount of time on wondering what other people think about you",
+
+  "Trees that are slow to grow bear the best fruit, so it's fine if you're stuck now",
+
+  "Sometimes we get so caught up in our own problems that we forget how amazing the world is",
+
+  "You know what, life happens, and it is okay.",
+
+  "Life has no remote.... get up and change it yourself!",
+
+  "Every solution to every problem is simple. It's the distance between the two where the mystery lies",
+  
+  "when you're scared but you still do it anyway, that's brave",
+  
+  "Sometimes, we must be willing to let go of the life we planned so as to have the life that is waiting for us",
+  
+  "If you want to be happy, be! (Easier said than done I know, but try!)",
+  
+  "Whatever you can do or dream you can, begin it.\nBoldness has genius, power and magic in it. Just start!",
+
+  "If I have figured something out from my 20 years on earth, it's that the future is unpredictable. As people said: It's like when your car slides on ice, you steer into the skid. (I have never done that before)",
+
+  "When painting, anything we don't like, we'll turn it into a happy little tree or something; We don't make mistakes, just happy little accidents",
+
+  "The secret to doing anything is believing that you can do it. Anything that you believe you can do strong enough, you can do. Anything. As long as you believe",
+
+  "All it takes is just a little change of perspective for us to begin to see a whole new world!",
+
+  "It's the imperfections that make something beautiful. That's what makes it different and unique from everything else. So don't worry if you feel like you can't do something right.",
+
+  "Just let go - and fall like a little waterfall!",
+  
+  "Don't lie to yourself, if you're sad, just be sad for a while and reset after!",
+  
+  "If you do something wrong, that is not a mistake.\nIf you do something wrong and do not correct it,\n now that is called a mistake..",
+  
+  "Sorrow prepares you for joy. It violently sweeps everything out of your house, so that new joy can find space to enter. It shakes the yellow leaves from the bough of your heart, so that fresh, green leaves can grow in their place. It pulls up the rotten roots, so that new roots hidden beneath have room to grow. Whatever sorrow shakes from your heart, far better things will take their place.",
+  
+  "Yell if you need it, and rant out loud if that's not enough!",
+
+  "Life is a series of natural and spontaneous changes. Don't resist them; that only creates sorrow. Let reality be reality. Let things flow naturally forward in whatever way they like",
+
+  "Sometimes doing something wrong is better than doing nothing at all",
+
+  "I know you're tired but come, this is the way.",
+
+  "In everybody's life there's a point of no return. And in a very few cases, a point where you can't go forward anymore. And when we reach that point, all we can do is quietly accept the fact. That's how we survive",
+
+  "I like to think that little by little I'll be able to solve my problems and survive",
+
+  "I think the most wasted of all days is one without laughter",
+  
+  "Life, I realize, was much like a song. In the beginning there is mystery, in the end there is confirmation, but it's in the middle where all the emotion resides to make the whole thing worthwhile",
+  
+  "Don't take life too seriously. Kick them in the balls when they need a good hit. they deserved it!",
+  
+  "That it will never come again is what makes life so sweet.",
+  
+  "Maybe we've got nothing to do today but smile?",
+
+  "You will find that it is necessary to let things go; simply for the reason that they are heavy..",
+
+  "Don't try to make life a mathematics problem with yourself in the center and everything coming out equal. When you're good, bad things can still happen. And if you're bad, you can still be lucky.",
+
+  "A learning experience is one of those things that says, “You know that thing you just did? Don't do that”",
+
+  "Maybe everyone can live beyond what they're capable of, and that includes you too",
+
+  "Of course you can have it all, but maybe not all at once, and that's still alright",
+
+  "I like flaws. I think they make things interesting",
+  
+  "The more you know who you are, and what you want, the less you let things upset you",
+  
+  "At the end of the day, let there be no excuses, no explanations, no regrets",
+  
+  "The biggest wall you have to climb is the one you build in your mind: Never let your mind talk you out of your dreams, trick you into giving up. Never let your mind become the greatest obstacle to success. To get your mind on the right track, the rest will follow.",
+  
+  "A heart has problems which mind cannot understand",
+
+  "I said to my soul, be still and wait without hope, for hope would be hope for the wrong thing; wait without love, for love would be love of the wrong thing; there is yet faith, but the faith and the love are all in the waiting. Wait without thought, for you are not ready for thought: So the darkness shall be the light, and the stillness the dancing.",
+
+  "Wanting to be someone else is a waste of who you are",
+
+  "There is a candle in your heart, ready to be kindled.\nThere is a void in your soul, ready to be filled.\nYou feel it, don't you?",
+
+  "Life sucks, and then we die... (I should stop joking right?)",
+
+  "No matter how careful you are, there's going to be the sense you missed something, the collapsed feeling under your skin that you didn't experience it all. There's that fallen heart feeling that you rushed right through the moments where you should've been paying attention. So let's just get used to that feeling.",
+
+  "There's nowhere you can be that isn't where you're meant to be...",
+
+  "We're killing time while we wait for life to shower us with meaning and happiness",
+  
+  "You'll never find a rainbow if you're looking down",
+  
+  "We are flawed creatures, all of us. Some of us think that means we should fix our flaws. But get rid of my flaws and there would be no one left",
+  
+  "When you do the right thing, you get the feeling of peace and serenity associated with it. Do it again and again",
+  
+  "Happiness is like a butterfly which, when pursued, is always beyond our grasp, but, if you will sit down quietly, may alight upon you",
+
+  "The best way out is always through",
+
+  "I am awfully greedy; I want everything from life. I want to be a woman and to be a man, to have many friends and to have loneliness, to work much and write good books, to travel and enjoy myself, to be selfish and to be unselfish… You see, it is difficult to get all which I want. And then when I do not succeed I get mad with anger. It doesn't make sense, but it's human.",
+
+  "I mean, I could and would die for you. But I couldn't, and wouldn't, live for you",
+
+  "Just because you think you can't, doesn't mean you shouldn't",
+
+  "Once you realize you deserve a bright future, letting go of your past is the best choice you will ever make",
+
+  "You don't have to take huge steps to change your life, even the smallest will do!",
+  
+  "Life is made of so many moments that mean nothing. Then one day, a single moment comes along to define every second that comes after… it's funny now that I think of it.",
+  
+  "They that can have patience can have what they will",
+  
+  "The universe is so well balanced that the mere fact that you have a problem also serves as a sign that there is a solution",
+  
+  "Maybe the reason birds can fly and we can't is simply because they have perfect faith, for to have faith is to have wings..",
+
+  "The greatest thing in the world is to know how to belong to oneself",
+
+  "In the name of any existing Gods, stop a moment, cease your work, look around you!",
+
+  "There is some kind of a sweet innocence in being human- in not having to be just happy or just sad- in the nature of being able to be both broken and whole, at the same time",
+
+  "Believe you can and you're halfway there",
+
+  "We are products of our past, but we don't have to be prisoners of it",
+
+  "Never let the future disturb you. You will meet it, if you have to, with the same weapons of reason which today arm you against the present",
+
+  "We try to fix the outside so much, but our control of the outer world is limited, temporary, and often, illusory",
+
+  "There are always answers. We just have to be smart enough, persistent enough, and want the answers enough",
+
+  "If your life has no problems, you're not really living it",
+  
+  "Every problem comes with a baggage of solutions",
+  
+  "Every man is a damn fool for at least five minutes every day; wisdom consists in not exceeding the limit",
+  
+  "If your feet are firmly planted on the ground you'll never be able to dance",
+  
+  "Despite our best efforts, we are going to be hurt when it's time for us to be hurt",
+
+  "sometimes you don't need a goal in life, you don't need to know the big picture. you just need to know what you're going to do next!",
+
+  "Sometimes it takes a good fall to really know where you stand",
+
+  "There are times to stay put, and what you want will come to you, and there are times to go out into the world and find such a thing for yourself",
+
+  "But how could you live and have no story to tell?",
+
+  "I believe in being strong when everything seems to be going wrong. I believe that happy girls are the prettiest girls. I believe that tomorrow is another day, and I believe in miracles actually..",
+
+  "Maybe I'll try anything once, twice if I like it, three times to make sure!",
+  
+  "Our life is what our thoughts make it",
+  
+  "Be melting snow, wash yourself of yourself",
+  
+  "Do all the good you can,\nBy all the means you can,\nIn all the ways you can,\nIn all the places you can,\nAt all the times you can,\nTo all the people you can,\nAs long as ever you can.",
+  
+  "If you do follow your bliss you put yourself on a kind of track that has been there all the while, waiting for you, and the life that you ought to be living is the one you are living. Follow your bliss and don't be afraid, and doors will open where you didn't know they were going to be",
+
+  "What would come, would come...and you would have to meet it, when it did",
+  
+  "Music melts all the separate parts of our bodies together",
+  
+  "Just be yourself cause I believe there is no one better",
+  
+  "Around here, however, we don't look backwards for very long. We keep moving forward, opening up new doors and doing new things, because we're curious...and curiosity keeps leading us down new paths",
+  
+  "The problems are solved, not by giving new information, but by arranging what we have known since long.",
+
+  "We can choose to throw stones, to stumble on them, to climb over them, or to build with them",
+
+  "Stories you read when you're the right age never quite leave you. You may forget who wrote them or what the story was called. Sometimes you'll forget precisely what happened, but if a story touches you it will stay with you, haunting the places in your mind that you rarely ever visit",
+
+  "Look at the stars. It won't fix the economy. It won't stop wars. It won't give you flat abs, or even help you figure out your relationship. But it's important. It helps you to remember that you and your problems are both infinitesimally small and conversely, that you are a piece of an amazing and vast universe",
+
+  "I've never met a problem a proper chocolate-and-peanut-butter toast couldn't fix",
+
+  "I know you're scared sometimes, but you'll make it",
+
+  //This is the 200 mark
+
 ];
 
 // Function to handle key press events
@@ -222,7 +426,7 @@ document.getElementById('inputNumber').addEventListener('keypress', handleKeyPre
 // Function to display a quote based on the input number
 function displayQuote() {
   const inputNumber = parseInt(document.getElementById('inputNumber').value);
-  if (inputNumber >= 0 && inputNumber <= 100) {
+  if (inputNumber >= 0 && inputNumber <= 200) {
       const quoteIndex = inputNumber; // Adjust index to match array indexing
       const quoteDisplay = document.getElementById('quoteDisplay');
       const quoteText = quotes[quoteIndex];
@@ -240,7 +444,7 @@ function displayQuote() {
           quoteDisplay.innerText = quoteText;
       }); // Adjust delay time as needed (in milliseconds)
   } else {
-      alert("Please enter a number between 1-100.");
+      alert("Please enter a number between 1-200.");
   }
 }
 
